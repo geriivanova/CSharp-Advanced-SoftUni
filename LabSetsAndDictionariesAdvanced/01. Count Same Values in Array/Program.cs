344b0f4ce2320f9daa0cac4774318f915ea555fd
Gergana Ivanova <geri7423@abv.bg>
2024-03-18 22:16:20 +0200

    task 1

--- LabSetsAndDictionariesAdvanced/01. Count Same Values in Array/Program.cs
+++ LabSetsAndDictionariesAdvanced/01. Count Same Values in Array/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> rezult = new Dictionary<double, int>();
+
+            foreach (double number in numbers)
+            {
+                if(!rezult.ContainsKey(number))
+                {
+                    rezult[number] = 0;
+                }
+                rezult[number]++;
+            }
+
+            foreach (var rez in rezult)
+            {
+                Console.WriteLine($"{rez.Key} - {rez.Value} times");
+            }   
         }
     }
 }
