84340c1d81d380cabad32c34d821ba4b68a13b4e
Gergana Ivanova <geri7423@abv.bg>
2024-05-20 19:06:50 +0300

    new task create file

---
+++ LabStacksAndQueues/01. Reverse a String 2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Reverse_a_String_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
