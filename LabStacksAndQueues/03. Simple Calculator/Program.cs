bcc3f9cf9c8de12bd263ab076faf61d26c1046d0
Gergana Ivanova <geri7423@abv.bg>
2024-05-20 20:57:09 +0300

    task finish

--- LabStacksAndQueues/03. Simple Calculator/Program.cs
+++ LabStacksAndQueues/03. Simple Calculator/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+            var reverseString = input.Reverse();
+            Stack<string> stack = new Stack<string>(reverseString);
+            
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count > 0)
+            {
+                char symbol = char.Parse(stack.Pop());
+                int number = int.Parse(stack.Pop());
+
+                if(symbol == '+')
+                {
+                    result += number;
+                }
+                else if(symbol == '-')
+                {
+                    result -= number;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
