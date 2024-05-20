e97aa135f3606af62fb7974b4f24f65f5da3041c
Gergana Ivanova <geri7423@abv.bg>
2024-05-20 19:13:28 +0300

    Update Program.cs

--- LabStacksAndQueues/01. Reverse a String 2.0/Program.cs
+++ LabStacksAndQueues/01. Reverse a String 2.0/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            Stack<char> stack = new Stack<char>(Console.ReadLine().ToCharArray());
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
         }
     }
 }
