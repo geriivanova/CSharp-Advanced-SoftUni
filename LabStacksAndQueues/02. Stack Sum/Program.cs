d572713fddc5d371522a2972f900ee8b91da0628
Gergana Ivanova <geri7423@abv.bg>
2024-05-20 20:20:13 +0300

    Update Program.cs

--- LabStacksAndQueues/02. Stack Sum/Program.cs
+++ LabStacksAndQueues/02. Stack Sum/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string command = Console.ReadLine().ToLower();
             
+            while(command != "end")
+            {
+                string[] splited = command.Split();
+                int firstNumber = int.Parse(splited[1]);
+
+                if (splited[0] == "add")
+                {
+                    int secondNumber = int.Parse(splited[2]);
+                    stack.Push(firstNumber);
+                    stack.Push(secondNumber);
+                }
+                else if (splited[0] == "remove")
+                {
+                    if(numbers.Count >= firstNumber)
+                    {
+                        for(int i = 0; i < firstNumber; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+
         }
     }
 }
