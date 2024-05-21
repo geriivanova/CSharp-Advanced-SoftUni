38506b73e656da919d44dd49eaaee81c5aad9af3
Gergana Ivanova <geri7423@abv.bg>
2024-05-21 17:19:45 +0300

    finish task

--- LabStacksAndQueues/04. Matching Brackets/Program.cs
+++ LabStacksAndQueues/04. Matching Brackets/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string expression = Console.ReadLine();
+
+            Stack<int> openingBracket = new Stack<int>();
+            
+            for(int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openingBracket.Push(i);
+                }
+                else if(expression[i] == ')')
+                {
+                    int startIndex = openingBracket.Pop();
+                    int endIndex = i;
+
+                    string substring = expression.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+
         }
     }
 }
