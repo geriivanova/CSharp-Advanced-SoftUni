ca17ad0f9feba9bd454be0aaa37458b94d9e12af
Gergana Ivanova <geri7423@abv.bg>
2024-05-21 17:44:07 +0300

    finish task

--- LabStacksAndQueues/05. Print Even Numbers/Program.cs
+++ LabStacksAndQueues/05. Print Even Numbers/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(numbers);
+            Queue<int> even = new Queue<int>();
+
+            while (queue.Count > 0)
+            {
+                int currentNumber = queue.Dequeue();
+                if (currentNumber % 2 == 0)
+                {
+                    even.Enqueue(currentNumber);
+                }
+                
+            }
+            Console.WriteLine(string.Join(", ", even.ToArray()));
         }
     }
 }
