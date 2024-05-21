a8f2f141e72e003ebf11a1237f5bf0abefe4fa2c
Gergana Ivanova <geri7423@abv.bg>
2024-05-21 18:16:38 +0300

    finish

--- LabStacksAndQueues/06. Supermarket/Program.cs
+++ LabStacksAndQueues/06. Supermarket/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<string> names = new Queue<string>();
+
+            string input = Console.ReadLine();
+            int namesCount = 0;
+
+            while (input != "End")
+            {
+               if(input != "Paid")
+               {
+                    names.Enqueue(input);
+                    namesCount++;
+               }
+               else
+               {
+                    while(names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                    namesCount = 0;
+               }
+               input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{namesCount} people remaining.");
         }
     }
 }
