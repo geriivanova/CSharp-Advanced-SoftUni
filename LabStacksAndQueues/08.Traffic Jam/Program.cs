37c8544d307e0c6898ac9b594f697ff5840bf892
Gergana Ivanova <geri7423@abv.bg>
2024-05-26 23:16:46 +0300

    create

---
+++ LabStacksAndQueues/08.Traffic Jam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08.Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
