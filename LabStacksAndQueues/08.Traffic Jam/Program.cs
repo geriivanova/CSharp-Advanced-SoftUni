85858bdfbf1c5692ab22422cdd592385aeb292d1
Gergana Ivanova <geri7423@abv.bg>
2024-05-26 23:43:13 +0300

    finish

--- LabStacksAndQueues/08.Traffic Jam/Program.cs
+++ LabStacksAndQueues/08.Traffic Jam/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            Queue<string> cars = new Queue<string>();
+            int passedCarsNumber = 0;
+
+            while((input = Console.ReadLine()) != "end")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                }
+                else if(input == "green")
+                {
+                    for(int i = 0; i < n; i++)
+                    {
+                        if(cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            passedCarsNumber++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"{passedCarsNumber} cars passed the crossroads.");
         }
     }
 }
